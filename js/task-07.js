57737bc58aa24a782214ea130bd328a581e5b331
chiri03 <lena.kiriac.97@gmail.com>
2023-05-07 00:03:24 +0300

    ex.8

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
-const fontSizeControl = document.querySelector("#font-size-control");
+const rangeControsl = document.querySelector("#font-size-control");
 const fontSizeText = document.querySelector("#text");
 
-fontSizeControl.addEventListener("input", () => {
-  fontSizeText.style.fontSize = `${fontSizeControl.value}px`;
+rangeControsl.addEventListener("input", () => {
+  fontSizeText.style.fontSize = `${rangeControsl.value}px`;
 });
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector("form.login-form");
 
+form.addEventListener("submit", (event) => {
+  let email = form.elements.email.value;
+  let password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("All field must be filled  ");
+  }
+
+  const userData = { email, password };
+  console.log(userData);
+  event.preventDefault();
+  form.reset();
+});
