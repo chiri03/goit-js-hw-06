1e2a3e21f0c7d3b36762496a9cbc0bd75a93122c
chiri03 <lena.kiriac.97@gmail.com>
2023-05-08 21:29:23 +0300

    ex.9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const names = {
+  body: document.querySelector("body"),
+  color: document.querySelector(".color"),
+  changeColor: document.querySelector(".change-color"),
+};
+
+console.log();
+
+names.changeColor.addEventListener("click", (randomColorChange) => {
+  names.body.style.background = `${getRandomHexColor()}`;
+  names.color.textContent = getRandomHexColor();
+});
